1f292b093d1450ed5f97a46b4c464b729f6b108d
Altemode <send@geth.gr>
2022-07-09 21:43:13 +0300

    3 commit

--- Indroduction.py
+++ Indroduction.py
@@ -6,7 +6,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Get Help1': 'https://www.extremelycoolapp.com/help',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
         'About': "# This is a header. This is an *extremely* cool app!"
     }
