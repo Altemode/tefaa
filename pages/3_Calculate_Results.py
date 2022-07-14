38721769e881d56d0391e9c1cd458bcb6eb0c08e
Altemode <send@geth.gr>
2022-07-14 14:56:09 +0300

    13 commit

--- pages/3_Calculate_Results.py
+++ pages/3_Calculate_Results.py
@@ -497,10 +497,10 @@ if url_list:
     
     jump_depending_impluse = float("nan")
     
-
-    #vertical_take_off_velocity = st.number_input("Give the time of vertical take off velocity")
-    jump_depending_take_off_velocity = (df.loc[take_off_time, 'Velocity'] ** 2) / (2 * 9.81)
-    jump_depending_time_in_air = (1 / 2) * 9.81 * (((landing_time - take_off_time) / 1000 ) / 2 ) ** 2 
+    if url_list[0]['type_of_trial'] == "CMJ":
+        #vertical_take_off_velocity = st.number_input("Give the time of vertical take off velocity")
+        jump_depending_take_off_velocity = (df.loc[take_off_time, 'Velocity'] ** 2) / (2 * 9.81)
+        jump_depending_time_in_air = (1 / 2) * 9.81 * (((landing_time - take_off_time) / 1000 ) / 2 ) ** 2 
 
     ######### ###### ######### ######## BRUSHED AREA ########### ########## ###########
     if brushed_submitted:
